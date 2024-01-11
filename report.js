d617f589036126322b9f64bf4edca0a660ad6a39
Priyanka Soni <Priyankasoni2016@gmail.com>
2024-01-11 19:56:28 +0530

    report.js modified

--- report.js
+++ report.js
@@ -1,9 +1,13 @@
 function sortPages(pages)
       {
-        return []
+       const pagesArr = Object.entries(pages)
+       pagesArr.sort( (a,b)=> {
+             aHits = a[1]
+             bHits = b[1]
+             return b[1] - a[1]
+       })
       }
-             
-
+      
       module.exports = {
-         sortPages = 
+         sortPages 
       }
