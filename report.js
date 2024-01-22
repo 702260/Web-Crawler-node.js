82008edfc2f87201d2a65368b6250298614a94d6
Priyanka Soni <Priyankasoni2016@gmail.com>
2024-01-22 19:37:26 +0530

    main.js & report.js modified

--- report.js
+++ report.js
@@ -2,6 +2,16 @@ function printReport(pages)
       {
         console.log("=========")
         console.log("REPORT")
+        console.log("=========")
+        const sortedPages = sortPages(pages)
+        for(const sortedPage of sortedPages) {
+            const url = sortedPage[0]
+            const hits = sortedPage[1]
+            console.log('Found ${hits} links to page: ${url}')
+        }
+            console.log("=========")
+            console.log("END REPORT")
+            console.log("=========")
       }
      
       function sortPages(pages)
@@ -16,5 +26,6 @@ function printReport(pages)
       }
       
       module.exports = {
-         sortPages 
+         sortPages,
+         printReport
       }     
